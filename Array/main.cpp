43c25e72bdd888b479f4b8644e41ce8ae11e00fb
ls123456789 <2953757467@qq.com>
2015-12-09 18:11:32 +0800

    数组越界
    先定义两个int型的数组，大小都为4，给其都赋值为0。再给数组下标为4的变量赋值。然后输出数组a和b的分配空间范围，然后再输出a［0］，b［0］
    的值和越界的a［4］，b［4］的值。运行后发现，有时给两个数组的分配的空间是相连的。输出的a［0］的值与b［4］的值相同，都为程序中赋给b［4］
    的值，而a［4］的值变为0，b［0］的值变为给a［4］赋的值，相当于将名字与空间向后错位。

--- Array/main.cpp
+++ Array/main.cpp
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4]={0},b[4]={0};
+    a[4]=123;
+    b[4]=456;
+    printf("a=%x &a[3]=%x b=%x &b[3]=%x\n",a,&a[3],b,&b[3]);
+    printf("a[0]=%d a[4]=%d b[0]=%d b[4]=%d\n",a[0],b[0],a[4],b[4]);
     return 0;
 }
